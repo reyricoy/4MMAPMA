7019e79434c1f47205e8705a0676539c68157c8c
Tanguybes <44745521+Tanguybes@users.noreply.github.com>
2018-11-04 12:41:07 +0100

    Add files via upload

---
+++ dico.c
@@ -0,0 +1,88 @@
+#include "dico.h"
+
+dico create_dico()
+{
+  dico dict=calloc(NB_KEYS,sizeof(tree));  //allocation d'un tableau de 26 arbres
+  return dict;
+}
+
+
+void destroy_dico(dico * d)
+{
+  if(d==NULL)
+  {
+    return;
+  }
+  for(int i=0;i<NB_KEYS;i++)
+  {
+    destroy_dico(&((*d[i])->children)); //On free tous les sous-dicos, et ensuite on remonte
+  }
+  free(d);
+}
+
+
+unsigned nb_children(tree arbre)
+{
+  int compteur;
+  for(int i=0;i<NB_KEYS;i++)
+  {
+    if(arbre[i].children!=NULL)
+    {
+      compteur++;
+    }
+  }
+  return compteur;
+}
+
+unsigned nb_nodes(dico d)
+{
+  unsigned noeuds;
+  if (d==NULL)
+  {
+    return 0;
+  }
+  for(int i=0;i<NB_KEYS;i++)
+  {
+    noeuds+=nb_nodes(d[i]->children);
+  }
+  return noeuds+1;
+}
+
+unsigned height(dico d)
+{
+  if (d==NULL)
+  {
+    return 0;
+  }
+  unsigned hauteur=height(d[0]->children);
+  for(int i=0;i<NB_KEYS;i++)
+  {
+    hauteur=(hauteur>height(d[i]->children))?hauteur:height(d[i]->children);
+  }
+  return hauteur;
+}
+
+int main()
+{
+  dico dictionnaire;
+  dictionnaire=create_dico();
+  printf("oui");
+  node noeud;
+  noeud.first='a';
+  noeud.end_of_word=TRUE;
+  noeud.children=NULL;
+  printf("oui");
+  dictionnaire[0]=&noeud;
+  printf("oui");
+  node noeud2;
+  noeud2.first='b';
+  noeud2.end_of_word=FALSE;
+  noeud2.children=create_dico();
+  dictionnaire[1]=&noeud2;
+  node noeud3;
+  noeud3.first='a';
+  noeud3.end_of_word=TRUE;
+  noeud3.children=NULL;
+  (dictionnaire[1]->children)[0]=&noeud3;
+  destroy_dico(&dictionnaire);
+}
---
+++ dico.h
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#define TRUE 1
+#define FALSE 0
+
+
+struct node {
+    char first;
+    bool end_of_word;
+    struct node ** children;
+};
+typedef struct node  node;
+typedef struct node * tree;
+typedef struct node ** dico;
+
+#define NB_KEYS 26
+unsigned get_index(char c) { return c - 'a'; }
+char get_char(unsigned index) { return index + 'a'; }
+
+
+
+/*//************TRAVAIL 1************\\*/
+// construction
+dico create_dico();
+//destruction
+void destroy_dico(dico * d);
+
+
+/*//************TRAVAIL 2************\\*/
+
+/** mesures */
+unsigned nb_children(tree arbre);
+unsigned nb_nodes(dico d);
+unsigned height(dico d);
