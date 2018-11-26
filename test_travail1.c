91c437ebc9a54c306742e60f6dd22160a959b835
Tanguybes <44745521+Tanguybes@users.noreply.github.com>
2018-11-26 19:44:59 +0100

    Add files via upload

---
+++ test_travail1.c
@@ -0,0 +1,19 @@
+#include "dico.h"
+
+/*
+Le principe du travail 1 était la création du dictionnaire
+ainsi que sa destruction.
+
+*/
+int main()
+{
+  dico dictionnaire;
+  int test;
+
+  dictionnaire=create_dico();
+
+  test=destroy_dico(&dictionnaire);
+
+  printf("Le dico a bien ete free [%d]",test);
+  
+}
---
+++ test_travail_2.c
@@ -0,0 +1,18 @@
+#include "dico.h"
+
+/*
+Le principe du TRAVAIL 2 etait d'ecrire les primitives de
+manipulation d'un dictionnaire.
+
+Savoir le nombre de feuilles, de noeuds et la hauteur du dictionnaire.
+
+Pouvoir imprimer toutes les lettres du dictionnaire
+
+Pouvoir dire si deux dictionnaire sont egaux.
+*/
+
+int main()
+{
+  dico dictionnaire1, dictionnaire2;
+  
+}
